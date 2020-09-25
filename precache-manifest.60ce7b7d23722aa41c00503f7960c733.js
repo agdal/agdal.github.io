dab30fad07a83c64a40b39eecb6425564c1ee5c4
Oliver Agdal <oliverskole266@gmail.com>
2020-09-25 14:03:25 +0200

    first

---
+++ precache-manifest.60ce7b7d23722aa41c00503f7960c733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51be036ff0993404b1e72d0d7fd690bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b17ddaf9823bdef7480e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a1b0aedc7f9a6363ff9",
+    "url": "/static/js/2.9a799ab6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9a799ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17ddaf9823bdef7480e",
+    "url": "/static/js/main.1941bec3.chunk.js"
+  },
+  {
+    "revision": "4463920c9bcd6448dfaa",
+    "url": "/static/js/runtime-main.74e24f70.js"
+  }
+]);
